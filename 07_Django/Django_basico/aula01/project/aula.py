a5a55741b0524a58964045e4420295e9c45ac5ad
Alexandre White de mello <alexandrewhitedemello@hotmail.com>
2024-07-11 10:12:33 -0300

    new file:   07_Django/Django_basico/aula01/blog/__init__.py 	new file:   07_Django/Django_basico/aula01/blog/admin.py 	new file:   07_Django/Django_basico/aula01/blog/apps.py 	new file:   07_Django/Django_basico/aula01/blog/migrations/__init__.py 	new file:   07_Django/Django_basico/aula01/blog/models.py 	new file:   07_Django/Django_basico/aula01/blog/tests.py 	new file:   07_Django/Django_basico/aula01/blog/views.py 	new file:   07_Django/Django_basico/aula01/home/__init__.py 	new file:   07_Django/Django_basico/aula01/home/admin.py 	new file:   07_Django/Django_basico/aula01/home/apps.py 	new file:   07_Django/Django_basico/aula01/home/migrations/__init__.py 	new file:   07_Django/Django_basico/aula01/home/models.py 	new file:   07_Django/Django_basico/aula01/home/tests.py 	new file:   07_Django/Django_basico/aula01/home/views.py 	new file:   07_Django/Django_basico/aula01/manage.py 	new file:   07_Django/Django_basico/aula01/project/__init__.py 	new file:   07_Django/Django_basico/aula01/project/asgi.py 	new file:   07_Django/Django_basico/aula01/project/aula.py 	new file:   07_Django/Django_basico/aula01/project/settings.py 	new file:   07_Django/Django_basico/aula01/project/urls.py 	new file:   07_Django/Django_basico/aula01/project/wsgi.py

---
+++ 07_Django/Django_basico/aula01/project/aula.py
@@ -0,0 +1,17 @@
+# DOC DJANGO
+# https://docs.djangoproject.com/en/5.0/
+
+# Respostas HTTP
+# https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status
+
+# criar projeto django
+# python manage.py startproject project
+
+# django help
+# django-admin --help
+
+# para rodar o manage.py
+# python manage.py runserver
+
+# Criando 2 apps para blog e home
+# python manage.py startapp blog ou home
